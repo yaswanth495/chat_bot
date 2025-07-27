317b19fa3b66050f91f3e58004152218bc71e3cc
yaswanth495 <yaswanthsamudram144@gmail.com>
2025-07-27 19:29:34 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCHq6roc-ttBzqCuT2hJ5GI_a1VFo2_m-4")
+genai.configure(api_key="Enter your API key")
 model = genai.GenerativeModel('gemini-1.5-flash')
 chat = model.start_chat(history=[])
 
